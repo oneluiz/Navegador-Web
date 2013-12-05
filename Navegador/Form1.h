7739a6c709b3f4fe4a9abc915cfb1a7746787b8a
One <projectonecore@gmail.com>
2013-12-04 23:03:51 -0600

    Agregando archivos del navegador

---
+++ Navegador/Form1.h
@@ -0,0 +1,81 @@
+#pragma once
+
+namespace Navegador {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén utilizando.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::WebBrowser^  webBrowser1;
+	protected: 
+
+	private:
+		/// <summary>
+		/// Variable del diseñador requerida.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido del método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
+			this->webBrowser1 = (gcnew System::Windows::Forms::WebBrowser());
+			this->SuspendLayout();
+			// 
+			// webBrowser1
+			// 
+			this->webBrowser1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->webBrowser1->Location = System::Drawing::Point(0, 0);
+			this->webBrowser1->MinimumSize = System::Drawing::Size(20, 20);
+			this->webBrowser1->Name = L"webBrowser1";
+			this->webBrowser1->Size = System::Drawing::Size(681, 498);
+			this->webBrowser1->TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(681, 498);
+			this->Controls->Add(this->webBrowser1);
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^  >(resources->GetObject(L"$this.Icon")));
+			this->Name = L"Form1";
+			this->Text = L"Navegador Web";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
+
---
+++ Navegador/Navegador.cpp
@@ -0,0 +1,18 @@
+// Navegador.cpp: archivo de proyecto principal.
+
+#include "stdafx.h"
+#include "Form1.h"
+
+using namespace Navegador;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	// Habilitar los efectos visuales de Windows XP antes de crear ningún control
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false); 
+
+	// Crear la ventana principal y ejecutarla
+	Application::Run(gcnew Form1());
+	return 0;
+}
